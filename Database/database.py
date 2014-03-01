b350a924e7f76a9d8734178714de53b38b04c421
Tiana Randriamparany <thinas4@gmail.com>
2014-03-01 12:46:23 +0100

    Suppression du champs nb_words dans Database

--- Database/database.py
+++ Database/database.py
@@ -1,16 +1,16 @@
 #-*- coding: utf8 -*-
-import sqlite3 as sq3
+import sqlite3
 
 class Database():
 	
 	def __init__(self, dataFile):
-		# Create database file's path
+		# Chemin du fichier de la base de donnees
 		self.dataFile = dataFile
-		# Create cursor and connection
-		self.conn = sq3.connect(self.dataFile)
+		# Creation de la connexion et du curseur
+		self.conn = sqlite3.connect(self.dataFile)
 		self.cur = self.conn.cursor()
 
-	# Delete tables
+	# Suppression des tables
 	def del_tables(self):
 		try:
 			self.cur.execute("DROP TABLE words")
@@ -19,29 +19,29 @@ class Database():
 		except:
 			print('*** Requete SQL incorrecte del_tables ***')
 
-	# Create tables
+	# Création des tables
 	def creat_tables(self):
 		try:
-			# Book table
-			self.cur.execute("""CREATE TABLE books(id_book INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(45), author VARCHAR(45), nb_words INTEGER)""")
-			# Word table
+			# Table des livres
+			self.cur.execute("""CREATE TABLE books(id_book INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(45), author VARCHAR(45))""")
+			# Table des mots
 			self.cur.execute("""CREATE TABLE words(id_word INTEGER PRIMARY KEY AUTOINCREMENT, val VARCHAR(45))""")
-			# TF table
+			# Table des TFs
 			self.cur.execute("""CREATE TABLE TF(id_book INTEGER NOT NULL, id_word INTEGER NOT NULL, val FLOAT, FOREIGN KEY (id_book) REFERENCES books(id_book), FOREIGN KEY (id_word) REFERENCES words(id_word), PRIMARY KEY (id_book, id_word))""")
 		except:
 			print('*** Requete SQL incorrecte creat_tables ***')
 
-	# Add a book
-	def add_book(self, title_book, author_book, nb_words):
+	# Ajout d'un livre
+	def add_book(self, title_book, author_book):
 		try:
-			self.cur.execute("""INSERT INTO books(title, author, nb_words) VALUES ('"""+title_book+"""', '"""+author_book+"""', """+str(nb_words)+""");""")
+			self.cur.execute("""INSERT INTO books(title, author) VALUES ('"""+title_book+"""', '"""+author_book+"""');""")
 		except:
 			print("*** Requete SQL incorrecte add_book("+title_book+") ***")
 		else:
 			print("add_book: "+title_book)
 			print
 
-	# Add a word
+	# Ajout d'un mot
 	def add_word(self, val_word):
 		try:
 			self.cur.execute("""INSERT INTO words(val) VALUES ('"""+val_word+"""')""")
@@ -51,7 +51,7 @@ class Database():
 			print("add_word: "+val_word)
 			print
 
-	# Find id_book
+	# Recherche id_book
 	def id_book(self, title_book, author_book):
 		try:
 			self.cur.execute("""SELECT id_book FROM books WHERE title='"""+title_book+"""' AND author='"""+author_book+"""'""")
@@ -62,7 +62,7 @@ class Database():
 			if row:
 				return row[0]
 
-	# Find id_word
+	# Recherche id_word
 	def id_word(self, word):
 		try:
 			self.cur.execute("""SELECT id_word FROM words WHERE val='"""+word+"""'""")
@@ -73,7 +73,7 @@ class Database():
 			if row:
 				return row[0]
 
-	# Add TF in TF table
+	# Ajout du TF d'un mot dans TF
 	def add_tf(self, id_book, id_word, tf_word):
 		try:
 			self.cur.execute("""INSERT INTO TF(id_book, id_word, val) VALUES ("""+str(id_book)+""", """+str(id_word)+""", """+str(tf_word)+""")""")
@@ -83,25 +83,25 @@ class Database():
 			print("add_tf: "+str(tf_word))
 			print
 
-	# Add a book in the database
-	def add_book_to_database(self, title_book, author_book, tf_words, nb_words):
-		# Find the book's id
+	# Ajout d'un livre dans la base de donnees
+	def add_book_to_database(self, title_book, author_book, tf_words):
+		# Recherche de l'id du livre
 		idb = self.id_book(title_book, author_book)
 		if idb == None:
-			self.add_book(title_book, author_book, nb_words)
+			self.add_book(title_book, author_book)
 			idb = self.id_book(title_book, author_book)
 		for word in tf_words:
-			# Find the word's id
+			# Recherche de l'id du mot
 			idw = self.id_word(word)
 			if idw == None:
-				# Add the word in database if it isn't already in it 
+				# Ajout du mot s'il n'y est pas encore
 				self.add_word(word)
 				idw = self.id_word(word)
 			self.add_tf(str(idb), str(idw), tf_words[word])
 
-	# Find the TF value of a word in a book
+	# Recherche du TF d'un mot dans un livre
 	def tf_word(self, word, title_book, author_book):
-		# Find word's id and book's id
+		# Recherche des id du livre et du mot
 		idw = str(self.id_word(word))
 		idb = str(self.id_book(title_book, author_book))
 		try:
@@ -117,7 +117,7 @@ class Database():
 
 
 
-	# Show all books
+	# Affichage des livres
 	def show_books(self):
 		try:
 			self.cur.execute("""SELECT * FROM books""")
@@ -129,7 +129,7 @@ class Database():
 				print(b)
 			print
 
-	# Show all words
+	# Affichage des mots
 	def show_words(self):
 		try:
 			self.cur.execute("""SELECT * FROM words""")
@@ -141,7 +141,7 @@ class Database():
 				print(a)
 			print
 
-	# Show all TFs
+	# Affichage des TFs
 	def show_TFs(self):
 		try:
 			self.cur.execute("""SELECT * FROM TF""")
@@ -153,7 +153,7 @@ class Database():
 				print(a)
 			print
 
-	# Execute a SQL request
+	# Exécution d'une requête SQL
 	def execute_sql(self, req):
 		try:
 			self.cur.execute(str(req))
@@ -165,15 +165,16 @@ class Database():
 				print (r)
 			print
 
-	# Save the actual state of database
+	# Sauvegarde de l'état actuel de la base de données
 	def save_database(self):
 		self.conn.commit()
 
-	# Close the connection
-	def close_connection(self):
+	# Fermeture de la connexion
+	def close_connection():
 		self.conn.close()
 
-	# Save database and close connection
+	# Demande de confirmation des changements
+	# et fermeture de la connexion
 	def confirm(self):
 		choice = input("""Enregistrer l'état actuel de la base de données ("o"/"n") ? """)
 		if choice[0] == "o" or choice[0] == "O":
