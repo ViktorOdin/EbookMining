d8e87c67bc242456ad6c46b65c4dd1054cd2ab71
Isabelle RICHARD <isabellerichard89@gmail.com>
2014-03-20 21:32:43 +0100

    Amélioration de l'interface graphique

--- src/main.py
+++ src/main.py
@@ -11,25 +11,25 @@ import os
 # Interface graphique
 from javax.swing import *
 from java.awt import *
+from java.awt.event import *
 from javax.swing.table import DefaultTableModel
 
-class MyExample:
-	def __init__(self, dic_books, mat_sim):
+# Variables globales
+global mat_sim, dic_books
+
+class ResultFrame:
+	def __init__(self, result):
 		# Propriétés de la fenêtre
-		frame = JFrame("EbookMining")
-		frame.setSize(1000, 500) # FIXME
+		frame = JFrame("Resultats de la recommandation")
+		frame.setSize(1000, 500)
 		frame.setLayout(BorderLayout())
 
 		# Variables de classe
 		self.dic_books = dic_books
 		self.mat_sim = mat_sim
-		self.tableData = []
-		for id_book in dic_books:
-			title, author = dic_books[id_book]
-			self.tableData.append([id_book, title, author])
 		colNames = ('id_book', 'Titre', 'Auteur')
-		dataModel = DefaultTableModel(self.tableData, colNames)
-		self.table = JTable(dataModel)
+		self.dataModel = DefaultTableModel(result, colNames)
+		self.table = JTable(self.dataModel)
  
 		scrollPane = JScrollPane()
 		scrollPane.setPreferredSize(Dimension(800,400))
@@ -39,9 +39,63 @@ class MyExample:
 		panel.add(scrollPane)
 
 		frame.add(panel, BorderLayout.CENTER)
+		frame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE)
+		frame.setVisible(True)
+
+class MainFrame:
+	def __init__(self, dic_books, mat_sim):
+		# Propriétés de la fenêtre
+		frame = JFrame("EbookMining")
+		frame.setSize(1000, 500)
+		frame.setLayout(BorderLayout())
+
+		# Variables de classe
+		self.dic_books = dic_books
+		self.mat_sim = mat_sim
+		# Tri des livres en fonction du titre
+		sorted_dic_books =  sorted(dic_books.items(), key=lambda x: x[1])
+		self.tableData = []
+		for book in sorted_dic_books:
+			id_book = book[0]
+			title, author = book[1]
+			self.tableData.append([id_book, title, author])
+		colNames = ('id_book', 'Titre', 'Auteur')
+		self.dataModel = DefaultTableModel(self.tableData, colNames)
+		self.table = JTable(self.dataModel)
+		# self.table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS)
+ 
+		scrollPane = JScrollPane()
+		scrollPane.setPreferredSize(Dimension(800,400))
+		scrollPane.getViewport().setView((self.table))
+
+		panel = JPanel()
+		panel.add(scrollPane)
+
+		button = JButton("►", actionPerformed=self.recommand)
+		panel.add(button)
+
+		frame.add(panel, BorderLayout.CENTER)
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
 		frame.setVisible(True)
 
+	def getSelectedBook(self):
+		"""Retourne l'identifiant du livre sélectionné"""
+		index = self.table.getSelectedRow()
+		return self.dataModel.getValueAt(index, 0)
+
+	def recommand(self, event):
+		"""Trie les livres par ordre de similarité et les envoie dans une table"""
+		id_book = self.getSelectedBook()
+		# Tri des valeurs en fonction de la similarité
+		dic_sim = mat_sim[id_book]
+		sorted_dic_sim =  sorted(dic_sim.items(), key=lambda x: x[1], reverse=True)
+		result = []
+		title, author = dic_books[id_book]
+		for sim in sorted_dic_sim:
+			cur_id_book = sim[0]
+			title, author = dic_books[cur_id_book]
+			result.append([cur_id_book, title, author])
+		ResultFrame(result)
 
 if __name__ == '__main__':
 
@@ -70,7 +124,7 @@ if __name__ == '__main__':
 			mat_sim[id_book_i] = {}
 		mat_sim[id_book_i][id_book_j] = similarity
 
-	MyExample(dic_books, mat_sim)
+	MainFrame(dic_books, mat_sim)
 
 	# # Boucle interactive : Renvoie la liste des livres les plus proches pour un identifiant donné
 	# total_top = 20
