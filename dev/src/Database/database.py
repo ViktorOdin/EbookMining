98e5e92600aa6fc8447ab592c97272a5058dbb2a
Isabelle RICHARD <isabellerichard89@gmail.com>
2014-05-04 10:53:52 +0200

    Resolve #3 - add GPL header to source files

--- dev/src/Database/database.py
+++ dev/src/Database/database.py
@@ -1,4 +1,27 @@
-#-*- coding: utf8 -*-
+# -*- coding: utf8 -*-
+#!/usr/bin/env python2.7
+#
+# EbookMining/dev/src/Database/database.py
+#
+# Copyright © 2014 Victor OUDIN <viktor.odex@gmail.com>
+# Copyright © 2014 Lala Tiana RANDRIAMPARANY <thinas4@gmail.com>
+# Copyright © 2014 Isabelle RICHARD <isabelle.richard89@gmail.com>
+#
+# This file is part of EbookMining.
+#
+#  EbookMining is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  EbookMining is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with EbookMining.  If not, see <http://www.gnu.org/licenses/>.
+
 import sqlite3
 
 # Temps d'attente de la libération du verrou, en secondes
